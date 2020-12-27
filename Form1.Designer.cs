24c3b2ea36e4b0dd9161056f79a8a8784701e217
Bishal Thapa <45414395+tbishal99@users.noreply.github.com>
2020-12-27 13:14:30 -0500

    ccommit

---
+++ Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace WindowsFormsApp7
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fromvalue = new System.Windows.Forms.TextBox();
+            this.tovalue = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // fromvalue
+            // 
+            this.fromvalue.Location = new System.Drawing.Point(202, 17);
+            this.fromvalue.Name = "fromvalue";
+            this.fromvalue.Size = new System.Drawing.Size(125, 27);
+            this.fromvalue.TabIndex = 0;
+            this.fromvalue.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.fromvalue.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            // 
+            // tovalue
+            // 
+            this.tovalue.Location = new System.Drawing.Point(445, 17);
+            this.tovalue.Name = "tovalue";
+            this.tovalue.Size = new System.Drawing.Size(98, 27);
+            this.tovalue.TabIndex = 1;
+            this.tovalue.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            this.tovalue.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox2_KeyDown);
+            this.tovalue.Resize += new System.EventHandler(this.Form1_Resize);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(72, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(124, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "From(In Degrees)";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(333, 17);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(106, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "To(In Degrees)";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            this.label2.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1125, 762);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tovalue);
+            this.Controls.Add(this.fromvalue);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox fromvalue;
+        private System.Windows.Forms.TextBox tovalue;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            // Get the current graphics object.
+            Graphics g = e.Graphics;
+            Pen RedPen = new Pen(Color.Red, 1 / g.DpiX); // Drawing the initial line to separate graph and text box.
+            g.DrawLine(RedPen, 5, 50, 1920, 50);
+
+            bool isNumericbox1 = double.TryParse(fromvalue.Text, out double n1);//checking if degree value is present
+            bool isNumericbox2 = double.TryParse(tovalue.Text, out double n2);//checking if degree value is present.
+
+            if (isNumericbox1 && isNumericbox2)//if both text boxes have data input.
+            {
+
+                graphPoints(n1,n2, g);
+            }
+
+            
+
+        }
+
+        void graphPoints(double n1, double n2, Graphics g)
+        {
+            // Clear the form space.
+            g.Clear(this.BackColor);
+           
+            // Determine the size of the area in which we will draw.
+            Size size = this.ClientSize;
+
+            // Get the max and min x and y values.
+            // Get the max and min x and y values.
+            double maxX = (n2/180.0); // maximum degree value of range
+            double maxY = 1.0;  // max possible value of sin(angles)
+            double minX =(n1/180.0);// start value of the angle range
+            double minY = -1.0;// min possible value of sin(angle)
+            double[] valuesx = new double[1920]; // creating 1920 random points for the graph
+            double[] valuesy = new double[1920];
+
+            
+            for (int value = 0; value < 1920; value = value + 1)
+            {
+                 valuesx[value] = (n1/180.0)+(value*(n2-n1)) / (180.0*1920.0);  //creating x values for all the points of graph
+                 valuesy[value] =  Math.Sin((valuesx[value] * 180) * (Math.PI) / (180.0)); // creating y values for all the points of graph.
+            }
+            
+
+            double[] px = new double[1920];  // Array of pixels
+            double[] py = new double[1920];
+
+            // Scale the points to pixels.
+            for (int ip = 0; ip < valuesx.Length; ip++)
+            {
+                    px[ip] = (valuesx[ip] - minX) / (maxX - minX) * size.Width;
+                    py[ip] = 100+((size.Height-120) - (valuesy[ip] - minY) / (maxY - minY) * (size.Height-120));// 100+ for gap of text boxes
+                // -120 to make sure 20 pixels are left below the graph.
+
+            }
+            Pen myPen = new Pen(Color.Red) ;
+            g.DrawLine(myPen, 5, 50, 1920, 50);
+            myPen.Color = System.Drawing.Color.Blue;
+            for (int ip = 1; ip < px.Length; ip++)
+            {
+                    // Why the casting???
+                    g.DrawLine(myPen, (float)px[ip - 1], (float)py[ip - 1], (float)px[ip], (float)py[ip]);
+            }
+            
+        }
+       
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            Invalidate();
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter) { Invalidate(); }
+        }
+
+        private void textBox2_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(  e.KeyCode == Keys.Enter) { Invalidate(); }
+           
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
